82f65af7d60b641f3c4d656d53afc0ac2927d524
ibrayo1 <ibrayo1128@gmail.com>
2019-09-20 23:13:47 -0500

    hide the x-axes since we dont really need em

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ server.listen(3000, () => {
 
 const SerialPort = require('serialport'); 
 const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort('COM3'); //Connect serial port to port COM3. Because my Arduino Board is connected on port COM3. See yours on Arduino IDE -> Tools -> Port
+const port = new SerialPort('COM4'); //Connect serial port to port COM3. Because my Arduino Board is connected on port COM3. See yours on Arduino IDE -> Tools -> Port
 const parser = port.pipe(new Readline({delimiter: '\r\n'})); //Read the line only when new line comes.
 
 parser.on('data', (rate) => { // Read data
